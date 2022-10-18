cf7ab540ba045f8d009778807eb4bb2f7568d098
Mihovil Staglicic <mstaglicic@hotmail.com>
2022-10-18 17:26:49 +0200

    Uploading changes , improving win case in game

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,7 @@ public class Main {
         //underscores that represent the letters of the word.
         //For example, for the word “computer” it should be like:
         // _ _ _ _ _ _ _
-        String[] list = {"potato", "estate", "energy", "length", "java", "system", "effort", "wealth", "nation",
-                "police", "family", "sister", "church", "worker", "studio", "dinner", "player", "singer",
-                "farmer"};
+        String[] list = {"potato"};
         System.out.print("     _________\n" +
                 "     |/      |\n" +
                 "     |      \n" +
@@ -39,12 +37,13 @@ public class Main {
         int guessLimit = 5;
         System.out.println("\tEnter a letter only : ");
         Scanner scanner = new Scanner(System.in);
-        //char usersGuess = scanner.next().charAt(0);
         while (!hiddenWord.toString().equals(randomWord)) {
             String userInput = scanner.nextLine();
             userInput = userInput.toLowerCase();
             char a = userInput.charAt(0);
 
+            // adding conditions for this game - if our list of previous attempts does not contain a
+            // new letter from user and if user still have remaining attempts then our code is running
             if (!charList.contains(a) && guessCount != guessLimit) {
                 for (int i = 0; i < randomWord.length(); i++) {
                     if (randomWord.charAt(i) == a) {
@@ -53,17 +52,29 @@ public class Main {
                     }
                     charList.add(a);
                 }
+                // just a print where user can see how many attempts he have
                 System.out.println("You have left  " + (guessLimit - guessCount) + " attempts.");
-                //System.out.println("YOU WIN ! ");
                 System.out.println(hiddenWord);
+                System.out.println();
+
                 guessCount++;
+
+                // try to complete a loop for when user win , in other words when randomWord is equal to
+                // hiddenWord
+                if (hiddenWord.toString().equals(randomWord)) {
+                    System.out.println("You win in " + guessCount + " attempts");
+                }
+                // in this line we should call a class Draw which has draws of all cases - NOT DONE YET
+
+                // next line is about condition if user try to input the same letter as before it gives him
+                // an error and does not count it in attempts
             } else if (charList.contains(a)) {
                 System.out.println(" You already entered this letter. Enter a new letter again:");
+
+                // user has six attempts and when he is left at 0 attempts , automatically the game is finished
             } else if (guessCount == guessLimit) {
                 System.out.println(" Sorry, you LOST.");
                 System.out.println(" The word is " + randomWord);
-            } else if (randomWord == hiddenWord.toString()) {
-                System.out.println(" You win");
             }
         }
     }
